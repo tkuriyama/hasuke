13d420ddf84022f606e6e1223068f0ed39df5342
tkuriyama <taro.kuriyama@gmail.com>
2020-11-23 20:48:55 -0500

    Commenting out Euterpea as OpenMidi is broken with Big Sur

--- src/Ukulele/Compilers/ToMidi.hs
+++ src/Ukulele/Compilers/ToMidi.hs
@@ -1,6 +1,13 @@
 module Ukulele.Compilers.ToMidi where
 
-import Euterpea
+-- import Euterpea
+-- import Euterpea.IO.MIDI (toMidi)
+
+import Ukulele.Ukulele as Uke
 
 --------------------------------------------------------------------------------
+
+scoreToMidi :: FilePath -> Score -> IO ()
+scoreToMidi _ _ = pure ()
+--  toMidi fpath (interpret score)
 
