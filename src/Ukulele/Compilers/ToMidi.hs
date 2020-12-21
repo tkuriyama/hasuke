8a84b453bb4b84d8ba14cc800517162ff4b86037
tkuriyama <taro.kuriyama@gmail.com>
2020-12-20 23:35:32 -0500

    first pass that typechecks

--- src/Ukulele/Compilers/ToMidi.hs
+++ src/Ukulele/Compilers/ToMidi.hs
@@ -3,7 +3,6 @@ module Ukulele.Compilers.ToMidi where
 import           Data.Ratio
 
 import           Euterpea
-import           Euterpea.IO.MIDI (writeMidi)
 
 import            Ukulele.Ukulele as Uke
 
@@ -19,7 +18,7 @@ scoreToMidi fpath (Score _ _ xs) =
   writeMidi fpath $ line $ map (interpret lowG) xs
 
 interpret :: Tuning -> Section -> Music (Pitch, Volume)
-interpret s = case s of
+interpret tuning s = case s of
   TextSection _ -> addVolume 0 $ rest 0  -- skip text sections
   ChordSection _ -> addVolume 0 $ rest 0 -- not (yet) implemented
   TabSection t num denom xs -> interpretTab tuning t num denom xs
@@ -31,21 +30,26 @@ interpretTab :: Tuning -> Maybe Uke.Tempo -> Int -> Maybe Int -> [Uke.Bar] ->
 interpretTab tuning mt _ md xs =
   instrument AcousticGuitarNylon $
   Euterpea.tempo t' $
-  line $ map (interpretBar tuning time) xs
+  line $ map (interpretBar tuning baseDur) xs
   where
     t' = case mt of
-           Just t -> (time / qn) * ((toInteger t) % 120)
+           Just t -> (baseDur / qn) * ((toInteger t) % 120)
            Nothing -> 1
-    time = case md of
-              Just denom -> 1 % (toInteger denom)
-              Nothing -> 1 % 4
+    baseDur = case md of
+                Just denom -> 1 % (toInteger denom)
+                Nothing -> 1 % 4
 
 interpretBar :: Tuning -> Rational -> Bar -> Music (Pitch, Volume)
-interpretBar tuning time (Bar _ aStr eStr cStr gStr) =
+interpretBar tuning baseDur (Bar _ aStr eStr cStr gStr) =
   h aStr aAp :=: h eStr eAp:=: h cStr cAp:=: h gStr gAp
   where
-    h notes ap = addVolume 100 $ interpretString ap time notes
+    h notes ap = addVolume 100 $ interpretString ap baseDur notes
     (aAp, eAp, cAp, gAp) = tuning
 
 interpretString :: AbsPitch -> Rational -> [Uke.Note] -> Music Pitch
-interpretString ap time notes = _
+interpretString ap baseDur notes = line $ map h notes
+  where
+    h (Uke.Rest restDur) =
+      rest (baseDur * (toInteger restDur % 1))
+    h (Uke.Note fret noteDur _) =
+      note (baseDur * (toInteger noteDur % 1)) (pitch $ ap + fret)
